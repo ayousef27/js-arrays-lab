d4e8fa007d2a91922088d3b84c0c8edbfa5111df
ayousef27 <ahmenero27@gmail.com>
2024-11-06 13:03:38 +0300

    ex10

--- app.js
+++ app.js
@@ -40,7 +40,46 @@ Complete Exercise 3 in the space below:
 //foods.unshift('taco')
 //const favFood = foods[1]
 //console.log('Exercise 4 result:', favFood)
-const foods = ['pizza', 'cheeseburger']
-foods.unshift('taco')
-foods.splice(2, 0, 'tofu')
-console.log('Exercise 5 result:', foods)
+//const foods = ['pizza', 'cheeseburger']
+//foods.unshift('taco')
+//foods.splice(2, 0, 'tofu')
+//console.log('Exercise 5 result:', foods)
+
+//const foods = ['pizza', 'cheeseburger']
+//foods.unshift('taco')
+//foods.splice(2, 0, 'tofu')
+//foods.splice(1, 1, 'sushi', 'cupcake')
+//console.log('Exercise 6 result:', foods)
+
+//const foods = ['pizza', 'cheeseburger']
+//foods.unshift('taco')
+//foods.splice(2, 0, 'tofu')
+//foods.splice(1, 1, 'sushi', 'cupcake')
+//const yummy = foods.slice(1, 3)
+//console.log('Exercise 7 result:', yummy)
+//const foods = ['pizza', 'cheeseburger']
+//foods.unshift('taco')
+//foods.splice(2, 0, 'tofu')
+//foods.splice(1, 1, 'sushi', 'cupcake')
+//const yummy = foods.slice(1, 3)
+//const soyIdx = foods.indexOf('tofu')
+//console.log('Exercise 8 result:', soyIdx)
+
+//const foods = ['pizza', 'cheeseburger']
+//foods.unshift('taco')
+//foods.splice(2, 0, 'tofu')
+//foods.splice(1, 1, 'sushi', 'cupcake')
+//const yummy = foods.slice(1, 3)
+//const soyIdx = foods.indexOf('tofu')
+//const allFoods = foods.join(' -> ')
+//console.log('Exercise 9 result:', allFoods)
+
+//const foods = ['pizza', 'cheeseburger']
+//foods.unshift('taco')
+//foods.splice(2, 0, 'tofu')
+//foods.splice(1, 1, 'sushi', 'cupcake')
+//const yummy = foods.slice(1, 3)
+//const soyIdx = foods.indexOf('tofu')
+//const allFoods = foods.join(' -> ')
+//const hasSoup = foods.includes('soup')
+//console.log('Exercise 10 result:', hasSoup)
