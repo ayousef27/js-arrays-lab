33903548f4a1425b8452a0e32065ee302d6bdc6b
ayousef27 <ahmenero27@gmail.com>
2024-11-07 04:47:09 +0300

    finish

--- app.js
+++ app.js
@@ -83,3 +83,73 @@ Complete Exercise 3 in the space below:
 //const allFoods = foods.join(' -> ')
 //const hasSoup = foods.includes('soup')
 //console.log('Exercise 10 result:', hasSoup)
+
+//const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90]
+//const odds = []
+
+//for (let i = 0; i < nums.length; i++) {
+//if (nums[i] % 2 !== 0) {
+//odds.push(nums[i])
+//}
+//}
+
+//console.log('Exercise 11 result:', odds)
+
+//const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90]
+
+//const fizz = []
+//const buzz = []
+//const fizzbuzz = []
+
+//for (let i = 0; i < nums.length; i++) {
+//if (nums[i] % 3 === 0) {
+//fizz.push(nums[i])
+//}
+//if (nums[i] % 5 === 0) {
+//buzz.push(nums[i])
+//}
+//if (nums[i] % 3 === 0 && nums[i] % 5 === 0) {
+//fizzbuzz.push(nums[i])
+//}
+//}
+
+//console.log('Exercise 12 Results:')
+//console.log('  fizz:', fizz)
+//console.log('  buzz:', buzz)
+//console.log('  fizzbuzz:', fizzbuzz)
+
+//const numArrays = [
+//[100, 5, 23],
+//[15, 21, 72, 9],
+//[45, 66],
+//[7, 81, 90]
+//]
+//const numList = numArrays[numArrays.length - 1]
+
+//console.log('Exercise 13 result:', numList)
+
+//const numArrays = [
+//[100, 5, 23],
+//[15, 21, 72, 9],
+//[45, 66],
+//[7, 81, 90]
+//]
+//const num = numArrays[2][1]
+
+//console.log('Exercise 14 result:', num)
+
+const numArrays = [
+  [100, 5, 23],
+  [15, 21, 72, 9],
+  [45, 66],
+  [7, 81, 90]
+]
+let total = 0
+
+for (let i = 0; i < numArrays.length; i++) {
+  for (let j = 0; j < numArrays[i].length; j++) {
+    total = numArrays[i][j] + total
+  }
+}
+
+console.log('Exercise 15 result:\n', total)
